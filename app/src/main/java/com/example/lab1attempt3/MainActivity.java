bf83b154c16c6515a613646f1e23af1f4e79aa31
stepan <stepan-alekseev-87@mail.ru>
2020-10-25 20:18:36 +0700

    Pulled CI yml file. Added min max functions and tests for them. Hopefully CI will work.

--- app/src/main/java/com/example/lab1attempt3/MainActivity.java
+++ app/src/main/java/com/example/lab1attempt3/MainActivity.java
@@ -11,4 +11,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /** Eactly this type of comments is needed to create javadocs
+     * The min function finds and returns
+     * a minimal of two integers.
+     * @param a is the first integer to compare
+     * @param b is the second integer to compare
+     * @return returns the minimum
+     */
+    public int min(int a, int b) {
+        if (a > b) return b;
+        else return a;
+    }
+
+    /**
+     * The max function finds and returns
+     * the maximum of two integers inserted
+     * @param a is the first integer to compare
+     * @param b is the second integer to compare
+     * @return this statement returns the maximum
+     */
+    public int max(int a, int b) {
+        if (a > b) return a;
+        else return b;
+    }
 }
